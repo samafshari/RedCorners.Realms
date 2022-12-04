932f2181e3c29d59fcabf761503c6dfe67465f73
s_a@live.com (R) <s_a@live.com>
2022-12-04 08:32:58 +0200

    v1.0.0

---
+++ RealmOperationException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RedCorners
+{
+    public class RealmOperationException : AggregateException
+    {
+        public RealmOperationException(string message) : base(message) { }
+        public RealmOperationException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
